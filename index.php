88f65771c617c3796b79170bfabcb2a4065e10cc
felixa1243 <felixarajiph@gmail.com>
2022-04-08 16:27:01 +0700

    fix layout

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $menu=$menu["menu"];
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-    <a class="navbar-brand" href="#">Pizzahub</a>
+    <a class="navbar-brand ml-3" href="#">Pizzahub</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -31,24 +31,25 @@ $menu=$menu["menu"];
 <!--Main-->
 <div class="container-fluid mt-3">
   <div class="row">
+    <div class="col">
   <h1 class="h1">All menu</h1>
+    </div>
   </div>
-  <?php foreach ($menu as $row):
-  ?>
   <div class="row">
-    <div class="col-md-3 mt-3">
+  <?php foreach ($menu as $row):?>
+    <div class="col-md-4 mt-3 pd-3">
       <div class="card">
           <img class="card-img-top" src="assets/img/pizza/<?= $row["gambar"]; ?>" >
             <div class="card-body">
               <h5 class="card-title"><?= $row["nama"] ?></h5>
               <p class="card-text"><?= $row["deskripsi"];?></p>
-              <h5 class="card-title"><?= $row["harga"];?></h5>
+              <h5 class="card-title">Rp.<?= $row["harga"];?>,-</h5>
               <a href="#" class="btn btn-primary">Beli sekarang!</a></a>
         </div>
       </div>
     </div>
-  </div>
   <? endforeach?>
+  </div>
 </div>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
